9b7201ec0d6dfacd509dfe9f665ee4b889b15290
Rene <renerkasper@gmail.com>
2020-12-14 16:46:01 -0300

    Desenvolvida e testada através do Insomnia a criação de usuários da API

--- hcode-api-users/app/Http/Controllers/UsersController.php
+++ hcode-api-users/app/Http/Controllers/UsersController.php
@@ -9,5 +9,14 @@ class UsersController extends Controller
 {
     public function index() {
         return User::all();
-    }    
+    }
+
+    public function store(Request $request) {
+        $user = User::create([
+            "name"=>$request->input("name"),
+            "email"=>$request->input("email")
+        ]);
+
+        return $user;
+    }
 }
